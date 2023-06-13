458ea85173367b08351c5c830912a3b4411c3f7a
Christopher Moore <christopher.e.moore@gmail.com>
2023-06-13 19:40:38 +0800

    switching to modules

--- www/websocket.js
+++ www/websocket.js
@@ -5,8 +5,8 @@ receive messages by providing an onMessage function
 requires my js-util project
 */
 
-ClientConn = makeClass({
-	init : function(args) {
+class ClientConn {
+	constructor(args) {
 		if (args.uri !== undefined) this.uri = args.uri;
 		if (this.uri === undefined) throw "expected uri";
 
@@ -20,7 +20,7 @@ ClientConn = makeClass({
 		if (args.ajaxProto !== undefined) this.ajaxProto = args.ajaxProto;
 		if (this.ajaxProto === undefined) this.ajaxProto = 'https';
 
-		var clientConn = this;
+		let clientConn = this;
 
 		//callback on received message
 		this.onMessage = args.onMessage;
@@ -32,18 +32,18 @@ ClientConn = makeClass({
 		this.sendQueue = [];
 
 		//register implementation classes
-		this.AsyncComm = makeClass();
+		class AsyncComm {};
+		this.AsyncComm = AsyncComm;
 
-		this.AsyncCommWebSocket = makeClass({
-			name : 'AsyncCommWebSocket',
-			super : clientConn.AsyncComm,
-			init : function(done) {
+		class AsyncCommWebSocket extends AsyncComm {
+			constructor(done) {
+				super();
 				this.connected = false;
 				this.reconnect(done);
-			},
-			reconnect : function(done) {
+			}
+			reconnect(done) {
 				if (this.connected) return;
-				var thiz = this;
+				let thiz = this;
 				this.ws = new WebSocket(clientConn.wsProto+'://'+clientConn.uri);
 				this.ws.onopen = function(evt) {
 //console.log('websocket onopen', evt);
@@ -60,12 +60,12 @@ console.log('error code', evt.code);
 				};
 				this.ws.onmessage = function(evt) {
 //console.log('websocket onmessage', evt);
-					var isblob = evt.data.constructor == Blob;
+					let isblob = evt.data.constructor == Blob;
 					if (isblob) {
 						// blob to text, because javascript is a trash language/API
-						var reader = new FileReader();
+						let reader = new FileReader();
 						reader.onload = function(e) {
-							var text = reader.result;
+							let text = reader.result;
 							clientConn.onMessage(text);
 						};
 						reader.readAsText(evt.data);
@@ -81,106 +81,108 @@ console.log('websocket onerror', arguments);
 //console.log('error code', evt.code);
 					throw evt;
 				};
-			},
-			send : function(data) {
+			}
+			send(data) {
 				this.ws.send(data);
 			}
-		});
+		}
+		AsyncCommWebSocket.prototype.name = 'AsyncCommWebSocket';
+		this.AsyncCommWebSocket = AsyncCommWebSocket;
 
-		this.AsyncCommAjax = makeClass({
-			//static variable
-			sessionID : undefined,
-
-			name : 'AsyncCommAjax',
-			super : clientConn.AsyncComm,
-			init : function(done) {
+		class AsyncCommAjax extends AsyncComm {
+			constructor(done) {
+				super();
+				this.sessionID = undefined;
 				this.connected = true;
 				this.sendQueue = [];
 				this.partialMsg = '';
 				this.poll();
 				if (done) done();
-			},
-			reconnect : function() {},	//nothing right now
-			poll : function() {
-				var thiz = this;
+			}
+			reconnect() {}	//nothing right now
+			poll() {
+				let thiz = this;
 				setTimeout(function() {
-					var sendQueue = thiz.sendQueue;
+					let sendQueue = thiz.sendQueue;
 					//cookies cross domain?  port change means domain change?  just wrap sessions into the protocol ...
 					if (thiz.sessionID !== undefined) {
 						sendQueue.splice(0, 0, 'sessionID '+thiz.sessionID);
 					}
 					thiz.sendQueue = [];
-					$.ajax({
-						type : 'POST',
-						url : clientConn.ajaxProto+'://'+clientConn.uri,
-						data : JSON.stringify(sendQueue),
-						//dataType : 'json',
-						success : function(msgsdata) {
-console.log('got '+msgsdata);
-							var msgs = $.parseJSON(msgsdata)
+					fetch(
+						clientConn.ajaxProto+'://'+clientConn.uri,
+					{
+						body : JSON.stringify(sendQueue),
+					}).then(response => {
+						if (!response.ok) throw 'not ok';
+console.log('got response',response);
+						response.json(msgs => {
+console.log('got msgs',msgs);
 							//process responses
-							for (var i = 0; i < msgs.length; i++) {
-								var msg = msgs[i];
+							for (let i = 0; i < msgs.length; i++) {
+								let msg = msgs[i];
 								if (msg.substring(0,10) == 'sessionID ') {
 //console.log("sessionID is", msg.substring(10));
 									thiz.sessionID = msg.substring(10);
 								} else if (msg.substring(0,10) == '(partial) ') {
-									var part = msg.substring(10);
+									let part = msg.substring(10);
 									thiz.partialMsg += part;
 								} else if (msg.substring(0,13) == '(partialEnd) ') {
-									var part = msg.substring(13);
-									var partialMsg = thiz.partialMsg + part;
+									let part = msg.substring(13);
+									let partialMsg = thiz.partialMsg + part;
 									thiz.partialMsg = '';
 									clientConn.onMessage(partialMsg);
 								} else {
 									clientConn.onMessage(msgs[i]);
 								}
 							}
-						},
-						error : function(handler, options, ex) {
-							console.log("ajax error "+handler.status+' '+options+' '+ex);
-						},
-						complete : function() {
 							thiz.poll();
-						},
-						timeout : 30000
+						});
+					}).catch(e => {
+						console.log("fetch error", e);
 					});
+						//timeout : 30000
+						// ... does fetch have no timeout otherwise?
+						// ... do I have to add 'AbortSignal.timeout(30000) to change the default?
 				}, 500);
-			},
-			send : function(msg) {
+			}
+			send(msg) {
 				this.sendQueue.push(msg);
 			}
-		});
+		}
+		AsyncCommAjax.prototype.name = 'AsyncCommAjax';
+		this.AsyncCommAjax = AsyncCommAjax;
 
 		//first try websockets ...
 		//mind you, the server only handles the RFC websockets
 		this.commClasses = [];
 		if (!args.disableWebsocket) this.commClasses.push(this.AsyncCommWebSocket);
 		if (!args.disableAjax) this.commClasses.push(this.AsyncCommAjax);
-	},
+	}
 
-	connect : function(done) {
+	connect(done) {
 		this.impl = undefined;
-		for (var i = 0; i < this.commClasses.length; i++) {
+		for (let i = 0; i < this.commClasses.length; i++) {
 			try {
+console.log("attempting class", this.commClasses[i].prototype.name);
 				this.impl = new this.commClasses[i](done);
-				//console.log('succeeded with', this.commClasses[i].prototype.name);
+console.log('succeeded with', this.commClasses[i].prototype.name);
 				break;
 			} catch (ex) {
-				console.log('conn init failed '+this.commClasses[i].prototype.name+' '+ex);
+console.log('conn init failed',this.commClasses[i].prototype.name, ex);
 			}
 		}
 		if (this.impl === undefined) throw 'failed to initialize any kind of async communication';
-	},
+	}
 
-	send : function(msg) {
+	send(msg) {
 		if (!this.impl || !this.impl.connected) {
 			this.sendQueue.push(msg);
 			//TODO and register a loop to check
 		} else {
 			if (this.sendQueue.length) {
-				var thiz = this;
-				$.each(this.sendQueue, function(i,msg) {
+				let thiz = this;
+				this.sendQueue.forEach(msg => {
 					thiz.impl.send(msg);
 				});
 				this.sendQueue = [];
@@ -188,4 +190,6 @@ console.log('got '+msgsdata);
 			this.impl.send(msg);
 		}
 	}
-});
+}
+
+export {ClientConn};
