7028eb434e2c413d32ab1a1817cd6cf099377c22
thenumbernine <christopher.e.moore@gmail.com>
2025-10-11 11:38:45 +0800

    using string.hex

--- digest.lua
+++ digest.lua
@@ -7,10 +7,9 @@ end
 if not digest then
 	local has, openssl_digest = pcall(require,'openssl.digest')	-- luaossl
 	if has then
-		local function bin2hex(c) return ('%02x'):format(c:byte()) end
 		digest = function(algo, str, bin)
 			local result = openssl_digest.new(algo):final(str)
-			if not bin then result = result:gsub('.', bin2hex) end
+			if not bin then result = string.hex(result) end
 			return result
 		end
 	end
