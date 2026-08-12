aca0c5aeb7b49f6db46e489a69a042c09919bc77
thenumbernine <christopher.e.moore@gmail.com>
2026-08-12 17:41:58 +0700

    added resty.openssl support

--- digest.lua
+++ digest.lua
@@ -1,15 +1,28 @@
-local string = require 'ext.string'
-
 local digest
-local has, crypto = pcall(require,'crypto')	-- luacrypto
+-- luacrypto
+local has, crypto = pcall(require,'crypto')
 if has then
 	digest = crypto.digest
 end
 if not digest then
-	local has, openssl_digest = pcall(require,'openssl.digest')	-- luaossl
+	-- luaossl
+	local has, openssl_digest = pcall(require,'openssl.digest')
 	if has then
+		local string = require 'ext.string'
 		digest = function(algo, str, bin)
 			local result = openssl_digest.new(algo):final(str)
+			if not bin then result = string.hex(result) end
+			return result
+		end
+	end
+end
+if not digest then
+	-- https://github.com/fffonion/lua-resty-openssl
+	local has, openssl_digest = pcall(require, 'resty.openssl.digest')
+	if has then
+		local string = require 'ext.string'
+		digest = function(algo, str, bin)
+			local result = assert(assert(openssl_digest.new(algo)):final(str))
 			if not bin then result = string.hex(result) end
 			return result
 		end
